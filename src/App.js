ce2e27c0a71788361b008a5f51eada53eac64ff4
EllyHowell <elly.howell@hotmail.com>
2022-09-10 18:06:51 +0100

    Add day zero + minor cosmetic changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import "./styles/Main.css";
+import "./styles/Reactive.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./elements/Header";
 import Footer from "./elements/Footer";
 import "./styles/HeaderFooter.css";
 
+import DayZero from "./elements/DayZero";
+import "./styles/Day.css";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      Hello World
+      <div id="Main" className="container container-fluid">
+        <DayZero />
+      </div>
       <Footer />
     </div>
   );
